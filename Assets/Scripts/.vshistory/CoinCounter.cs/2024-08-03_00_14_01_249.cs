a666616232fa8849d65669fbca77c72e4471322f
Ricky Wu <rickywu1231@gmail.com>
2024-08-03 00:29:35 -0400

    Implemented coin counter
    Implemented a coin counter in the UI that tracks the number of coins collected and the total coins in the map.

---
+++ Assets/Scripts/.vshistory/CoinCounter.cs/2024-08-03_00_14_01_249.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Script to display the number of coins collected and the total number of coins in the UI
+
+public class CoinCounter : MonoBehaviour
+{
+    public GameObject player; // For getting info about coins
+    public Text coinInfo; // Text displaying coin information
+    public int totalCoins; // Total number of coins in the map
+
+    private int coinsCollected; // Coins collected by the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinsCollected = 0;
+        coinInfo.text = "0/" + totalCoins.ToString() + " Coins";
+    }
+
+    // Update is called once per frame
+    public void AddCoin()
+    {
+        coinsCollected++;
+        coinInfo.text = coinsCollected.ToString() + "/" + totalCoins.ToString() + " Coins";
+    }
+}
